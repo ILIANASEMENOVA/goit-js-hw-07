d9282f65d44b03cc20163799e6ab261e326e833b
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2023-08-20 01:00:39 +0300

    summary

---
+++ js/01-gallery.js
@@ -0,0 +1,69 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на ul.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>; */
+
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = createMarkup(galleryItems);
+
+gallery.addEventListener("click", onGalleryClick);
+
+function createMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `
+     <li class="gallery__item">
+            <a class="gallery__link" href= "${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const bigImage = event.target.dataset.source;
+  console.log(bigImage);
+
+  const instance = basicLightbox.create(`
+    <img src=${bigImage}  alt="${event.target.alt}">
+`);
+
+  instance.show();
+}
